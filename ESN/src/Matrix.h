1457a88dbd0665a82a0e2974e62d9dc767f43be4
baz1 <baz1.github@gmail.com>
2015-07-14 22:59:57 +0200

    Matrix qdoc also started

--- ESN/src/Matrix.h
+++ ESN/src/Matrix.h
@@ -7,23 +7,50 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <string.h>
+
 #ifdef QT_VERSION
-#include <QtGlobal>
+  #include <QtGlobal>
+  #define ASSERT(x) Q_ASSERT(x)
 #else
-// TODO ASSERT macro
+  #define ASSERT(x) (void) 0
+  #ifndef NULL
+    #define NULL ((void*) 0)
+  #endif
 #endif
 
-#ifndef NULL
-#define NULL ((void*) 0)
-#endif
+#define ASSERT_INT(x) ASSERT((x) < (1ULL << (sizeof(int) * 8 - 1)))
 
 template <typename T> class Matrix
 {
 public:
-    explicit inline Matrix() : data(NULL), m(0), n(0) {}
+    inline Matrix() : data(NULL) {}
+    Matrix(const Matrix<T> &other);
+    inline ~Matrix();
 private:
-    T **data; // First index is the row index (~ Y axis)
+    T *data; // data[i * n + j] for the i-th row, j-th column
     int m, n; // m rows, n columns
 };
+
+template <typename T> Matrix<T>::Matrix(const Matrix<T> &other) : m(other.m), n(other.n), data(NULL)
+{
+    if (other.data)
+    {
+        ASSERT((m > 0) && (n > 0));
+        ASSERT_INT(((unsigned long long) m) * ((unsigned long long) n));
+        size_t size = m * n;
+        data = new T[size];
+        memcpy((void*) data, (void*) other.data, size);
+    }
+}
+
+template <typename T> inline Matrix<T>::~Matrix()
+{
+    if (data)
+    {
+        ASSERT((m > 0) && (n > 0));
+        delete[] data;
+    }
+}
 
 #endif // MATRIX_H
